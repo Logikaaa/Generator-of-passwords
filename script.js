eeca6a2dc29304c62a4bca332a3128768979c92d
Logikaaa <123231827+Logikaaa@users.noreply.github.com>
2025-05-11 11:24:13 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+let button = document.querySelector(".button")
+let v1 = document.querySelector(".v1")
+let v2 = document.querySelector(".v2")
+let v3 = document.querySelector(".v3")
+let v4 = document.querySelector(".v4")
+let symbols = ["abc","ABC",""]
+function generate_1(){
+    let symbol = Math.random - 1
+}
+button.addEventListener('click', generate_1)
